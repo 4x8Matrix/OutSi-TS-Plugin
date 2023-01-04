f42ce6b7bd36c124fd8739bc6d4c7268b04cc4e9
AsyncMatrix <Catbob614@gmail.com>
2023-01-04 19:11:35 +0000

    ✨ Updated to work with the Roblox-Studio plugin

--- src/core/NetworkInterface.ts
+++ src/core/NetworkInterface.ts
@@ -3,12 +3,12 @@ import { ExtensionInstance } from "../extension";
 
 import { onAction, onActionSchema, ActionInterface } from "./routes/onAction";
 import { onReport, onReportSchema, ReportInterface } from "./routes/onReport";
-import { onVersion, onVersionSchema, VersionInterface } from "./routes/onVersion";
+import { onIdentify, onIdentifySchema, IdentifyInterface } from "./routes/onIdentify";
 
 export default class {
 	extensionInstance: ExtensionInstance;
 
-	targetHost: string = "localhost";
+	targetHost: string = "127.0.0.1";
 	targetPort: number = 3012;
 
 	server: FastifyInstance;
@@ -17,12 +17,12 @@ export default class {
 		this.extensionInstance = extensionInstance;
 		this.server = fastify();
 
-		var instance = this;
+		let instance = this;
 
 		this.server.post("/api/report", { schema: onReportSchema }, (request: FastifyRequest<ReportInterface>, reply: FastifyReply) => onReport(instance, request, reply));
 		this.server.post("/api/action", { schema: onActionSchema }, (request: FastifyRequest<ActionInterface>, reply: FastifyReply) => onAction(instance, request, reply));
 
-		this.server.post("/api/version", { schema: onVersionSchema }, (request: FastifyRequest<VersionInterface>, reply: FastifyReply) => onVersion(instance, request, reply));
+		this.server.post("/api/identify", { schema: onIdentifySchema }, (request: FastifyRequest<IdentifyInterface>, reply: FastifyReply) => onIdentify(instance, request, reply));
 
 		this.extensionInstance.onExtensionReady.connect(() => this.server.listen({
 			host: this.targetHost,
