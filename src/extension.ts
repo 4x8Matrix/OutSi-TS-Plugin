28ecf484bc1077922a7c523c7284fa3950720ede
AsyncMatrix <Catbob614@gmail.com>
2023-01-08 13:46:42 +0000

    ♻️ Refactored the 'extension' instance

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,21 @@
 /**
  * @name OutSi
- * @author AsyncMatrix
- * @version 0.0.1
+ * @description OutSi is a Roblox -> Visual Studio Code output replication tool
 **/
 
-import { ActionObject, MessageObject } from './core/types';
+import { ExtensionContext } from "vscode";
+import { ExtensionInstance } from "./instance";
 
-import { ExtensionContext } from 'vscode';
-import { Signal } from "typed-signals";
-
-import { branchType as pluginBranch } from './enums/pluginBranch';
-
-import NetworkInterface from './core/NetworkInterface';
-import TerminalInterface from './core/TerminalInterface';
-
-
-export class ExtensionInstance {
-	name: string = "OutSi";
-	version: string = "0.0.1";
-	author: string = "AsyncMatrix";
-	description: string = "OutSi is a Roblox -> Visual Studio Code output replication tool";
-	branch: string = pluginBranch.development;
-
-	context: ExtensionContext;
-
-	networkInterface: NetworkInterface;
-	terminalInterface: TerminalInterface;
-
-	onExtensionReady: Signal<() => void> = new Signal();
-
-	onMessageReceived: Signal<(messageObject: MessageObject) => void> = new Signal();
-	onActionReceived: Signal<(actionObject: ActionObject) => void> = new Signal();
-
-	constructor(context: ExtensionContext) {
-		this.context = context;
-
-		this.networkInterface = new NetworkInterface(this);
-		this.terminalInterface = new TerminalInterface(this);
-
-		this.onExtensionReady.connect(() => console.log("Plugin Active!"));
-	}
-
-	onExtensionActivated() { this.onExtensionReady.emit(); }
-}
+let runtimeInstance: ExtensionInstance;
 
 export function activate(context: ExtensionContext) {
-	const extensionInstance: ExtensionInstance = new ExtensionInstance(context);
+	runtimeInstance = new ExtensionInstance(context);
 
-	extensionInstance.onExtensionActivated();
+	runtimeInstance.onExtensionActivated.emit();
+}
+
+export function deactivate() {
+	if (!runtimeInstance) { return; }
+
+	runtimeInstance.onExtensionDeactivated.emit();
 }
---
+++ src/instance.ts
@@ -0,0 +1,34 @@
+import { ExtensionContext } from "vscode";
+import { Signal } from "typed-signals";
+
+import { branch } from "./enumeration/branch";
+import { robloxLogObject, robloxActionObject } from "./types/robloxTypes";
+
+import NetworkComponent from "./components/networkComponent";
+import TerminalComponent from "./components/terminalComponent";
+
+export class ExtensionInstance {
+	branch: branch = branch.development;
+
+	version: string = "0.1.0";
+
+	context: ExtensionContext;
+
+	networkComponent: NetworkComponent;
+	terminalComponent: TerminalComponent;
+
+	onExtensionActivated: Signal<() => void> = new Signal();
+	onExtensionDeactivated: Signal<() => void> = new Signal();
+
+	onLogReceived: Signal<(messageObject: robloxLogObject) => void> = new Signal();
+	onActionReceived: Signal<(actionObject: robloxActionObject) => void> = new Signal();
+
+	constructor(context: ExtensionContext) {
+		this.context = context;
+
+		this.networkComponent = new NetworkComponent(this);
+		this.terminalComponent = new TerminalComponent(this);
+ 
+		this.onExtensionActivated.connect(() => console.log("Plugin Active!"));
+	}
+}
